6ee47269e8b6133492bda6f5fdeccfcd7b2fcd91
Ayush Kumar <raj6669ayush@gmail.com>
2022-11-13 15:47:27 +0000

    Added JS functionality to change shape

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -176,6 +176,14 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("./styles.css");
+var cur = "square";
+var shape = ["square", "rectangle", "circle", "oval", "triangle-up", "triangle-down", "triangle-topleft", "triangle-bottomleft", "triangle-bottomright", "triangle-right"];
+var color = ["red", "orange", "green", "maroon", "pink", "violet"];
+document.getElementById("shape").onclick = function () {
+  var rand = shape[Math.floor(Math.random() * shape.length)];
+  document.getElementById(cur).setAttribute("id", rand);
+  cur = rand;
+};
 },{"./styles.css":"src/styles.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+
+var cur = "square";
+
+var shape = ["square", "rectangle", "circle", "oval", "triangle-up", "triangle-down", "triangle-topleft", "triangle-bottomleft", "triangle-bottomright", "triangle-right"];
+
+var color = ["red", "orange", "green", "maroon", "pink", "violet"];
+
+
+document.getElementById("shape").onclick = function () {
+    var rand = shape[Math.floor(Math.random() * shape.length)];
+    document.getElementById(cur).setAttribute("id", rand);
+    cur = rand;
+}
